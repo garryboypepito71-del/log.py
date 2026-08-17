85d9a874b48f49250ed613f01cae61edcced9020
garryboypepito71-del <garryboypepito71@gmail.com>
2026-08-17 14:36:16 +0000

    sc

--- log.py
+++ log.py
@@ -732,9 +732,13 @@ section[data-testid="stSidebar"]::-webkit-scrollbar-thumb{background:linear-grad
 """, unsafe_allow_html=True)
 st.markdown("""
 </div>
+</div>
 """, unsafe_allow_html=True)
 
+
 st.markdown("""
+</div>
+</div>
 <div class="headbar-container">
   <div class="headbar-card">
     <div class="headbar-title"><span class="logo-mark">AH</span> Ailyn Project Management System</div>
